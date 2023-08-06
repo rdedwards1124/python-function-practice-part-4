141fdd8763bebb7a0ac69406783f282c3bbcb0b6
Rashonda Edwards <rashondaedwards@aol.com>
2023-08-05 21:59:40 -0700

    final commit

--- index.py
+++ index.py
@@ -36,8 +36,34 @@ mult_list(my_list)
 
 # Write a Python function called rev_string() to reverse a string.
 
+def rev_string(string):
+    string2 = string[::-1]
+    print(f"{string} reversed is: {string2}")
+
+rev_string("Hello World")
+
 
 # Write a Python function called num_within() to check whether a number falls in a given range.
+def num_within(a, b, c):
+    if a >= b and a <= c:
+        print(f"TRUE!!! {a} does fall in the range from {b} to {c}")
+    else:
+        print(f"FALSE!!! {a} does NOT fall in the range from {b} to {c}")
+
+num_within(9, 1, 10)
 
 
 # Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+
+def pascal(n):
+    def calculate_pascal_value(row, col):
+        if col == 0 or col == row:
+            return 1
+        return calculate_pascal_value(row - 1, col - 1) + calculate_pascal_value(row - 1, col)
+
+    for i in range(n):
+        for j in range(i + 1):
+            print(calculate_pascal_value(i, j), end=" ")
+        print()
+
+pascal(5)
